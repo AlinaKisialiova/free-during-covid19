a4693658db3cbc7b20b935468caaeb98094e82f6
Alina <aflammaal@gmail.com>
2020-03-31 23:35:41 +0100

    Create project structure and add settings.

---
+++ src/main/java/by/akisialiova/freedrngcovid19/FreeDrngCovid19Application.java
@@ -0,0 +1,12 @@
+package by.akisialiova.freedrngcovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeDrngCovid19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeDrngCovid19Application.class, args);
+    }
+}
